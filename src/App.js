c50ae179d2a864b382bd73847c1163157724799e
Vilson Borges <vilsonborges98@gmail.com>
2020-08-04 19:33:20 -0300

    Hook useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd(){
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <ul>
+        {tech.map(tec => (
+          <li key={tec}>{tec}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+          Adicionar
+      </button>
+    </>
   );
 }
 
