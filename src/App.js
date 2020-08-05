753db1688e76ad9b02c92ded420d9484e4bdec8c
Vilson Borges <vilsonborges98@gmail.com>
2020-08-04 21:02:45 -0300

    hooks useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd(){
     setTech([...tech, newTech]);
     setNewTech('');
   }
+
+  useEffect(() => {
+    /**
+     * Running once as a 'did mount' 
+     * looking for localStorage data when 
+     * the component is created
+     */
+    const storageTech = localStorage.getItem('tech');
+
+    if(localStorage){
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+
+  useEffect(() =>{
+    /** 
+     * Monitoring data from 
+     * the 'tech' variable 
+     * as a 'did update'
+    */
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
   return (
     <>
       <ul>
